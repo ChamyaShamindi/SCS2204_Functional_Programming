05aba312b83bfd2f122fef62a6cf3b1ce9efef52
ChamyaShamindi <91967400+ChamyaShamindi@users.noreply.github.com>
2022-07-17 08:20:31 +0530

    Delete Tutorial 3 directory

--- Tutorial 3/Q1.scala
+++
@@ -1,4 +0,0 @@
-object are extends App{
-    def diskArea(r:Int):Double = math.Pi*r*r;
-    println("Area of a disk: " + diskArea(5));
-}
--- Tutorial 3/Q3.scala
+++
@@ -1,4 +0,0 @@
-object volume extends App{
-    def volSphere(r:Int):Double = (4/3)*math.Pi*r*r*r;
-    println("Volume of sphere: " + volSphere(5));
-}

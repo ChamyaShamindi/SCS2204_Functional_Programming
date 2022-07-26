0f4f23bc3ebee0dd5715f5883f49b44c43326c1a
ChamyaShamindi <91967400+ChamyaShamindi@users.noreply.github.com>
2022-07-26 23:40:43 +0530

    Update Q2.scala

--- Tutorial_3/Q2.scala
+++ Tutorial_3/Q2.scala
@@ -1,5 +1,5 @@
 object findTemp extends App{
     def cenToFan(c:Double):Double = c*1.8000 + 32.00;
     println("Celsius value is: 35C");
-    println("Fahrenheit value is: " + cenToFan(40));
+    println("Fahrenheit value is: " + cenToFan(35));
 }
